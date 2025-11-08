76a691ba7b95f783b23c32d9a1a00d456ff4a9fe
seyha yoeun <yoeunseyha11@gmail.com>
2025-11-08 23:02:09 +0700

    finished navbar and sidebar

--- scripts/layout.js
+++ scripts/layout.js
@@ -1,5 +1,4 @@
 const sidebar = document.querySelector(".js-sidebar");
-
 const sidebarToggleButton = document.querySelector(".js-sidebar-toggle");
 
 sidebarToggleButton.addEventListener("click", () => {
