93c8c283555a30b17d1560aab679680c579b4d71
seyha yoeun <yoeunseyha11@gmail.com>
2025-11-08 20:45:29 +0700

    Created Navbar

---
+++ scripts/layout.js
@@ -0,0 +1,7 @@
+const sidebar = document.querySelector(".js-sidebar");
+
+const sidebarToggleButton = document.querySelector(".js-sidebar-toggle");
+
+sidebarToggleButton.addEventListener("click", () => {
+  sidebar.classList.toggle("collapsed");
+});
